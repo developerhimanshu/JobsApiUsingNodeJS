309433bd9622d620a6534dba7a3b9271c11be4a5
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-08-05 22:17:25 +0530

    Added the auth model and created register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,20 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors/index");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const register = async (req, res) => {
-  res.send("register user");
+  const user = await User.create({ ...req.body });
+  const token = jwt.sign(
+    { userId: user._id, userName: user.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
