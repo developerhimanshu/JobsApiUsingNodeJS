d0319107b7472bd9ee285698487a2fb1ecff6a13
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-08-07 17:33:08 +0530

    Created mongo db instance methods for better code optimization and added some login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,27 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors/index");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { BadRequestError, UnauthenticatedError } = require("../errors/index");
+
 require("dotenv").config();
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = jwt.sign(
-    { userId: user._id, userName: user.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "30d",
-    }
-  );
+  const token = user.createJWT();
+
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please enter a email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
