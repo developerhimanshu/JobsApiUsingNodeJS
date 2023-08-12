0805a594f9830fc2feffbf80bf27b2865299338f
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-08-12 08:11:49 +0530

    Added a security layer using rater limiter

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 require("express-async-errors");
 
 //Extra Security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
 const app = express();
@@ -16,8 +20,19 @@ const jobRoute = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    mas: 100,
+  })
+);
+
+app.set("trust proxy", 1);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRoute);
